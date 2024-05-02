f8742d5d82800e8779f26c844d5433ea0958c24f
DiamondCodingYT <37113804+DiamondCodingYT@users.noreply.github.com>
2024-05-02 02:54:15 +0200

    [W02] Logging Testes now also pass when logging the last step (#5)
    As described here
    https://github.com/nilsreichardt/gad24-tests/issues/4#issuecomment-2083655664
    the logging test currently does not allow logging the last step when
    from and to index are equal, but this is considered passing by the
    Artemis Tests.
    This PR fixes this by sometimes having two options for valid steps. The
    same system could also be used to allow steps taken when ceiling the
    middle index instead of flooring.
    
    Fixes #4

--- w02/test/gad/logging/TestResultImplementation.java
+++ w02/test/gad/logging/TestResultImplementation.java
@@ -2,18 +2,28 @@ package gad.logging;
 
 import gad.binarysearch.Result;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestResultImplementation implements Result {
-    private int[] steps;
+
+    private final int[] steps;
     private int index;
 
+    public TestResultImplementation(int[][] possibleSolutions) {
+        this(Arrays.stream(possibleSolutions)
+                .mapToInt(s -> s.length)
+                .max().orElse(0));
+    }
     public TestResultImplementation(int expectedSize) {
         steps = new int[expectedSize];
     }
 
     @Override
     public void addStep(int step) {
+        System.out.println("added step to index " + step);
         if (index < steps.length) {
             steps[index] = step;
         }
@@ -25,4 +35,34 @@ public class TestResultImplementation implements Result {
 
         assertArrayEquals(solution, steps, "Logged wrong path");
     }
+
+    public void testLogging(int[][] possibleSolutions) {
+        testLogging(possibleSolutions, "");
+    }
+    public void testLogging(int[][] possibleSolutions, String messagePrefix) {
+        assertTrue(Arrays.stream(possibleSolutions).anyMatch(
+                        solution -> index == solution.length && testArrayEquals(solution, steps)
+                ),
+                messagePrefix + "steps did not match any valid solution (possibilities are: " +
+                        Arrays.stream(possibleSolutions)
+                                .map(Arrays::toString)
+                                .collect(Collectors.joining(" or ")) +
+                        ")");
+    }
+
+    /**
+     * assumes arrays have same length
+     * @param a first array
+     * @param b second array
+     * @return whether array a and b are equal
+     */
+    private boolean testArrayEquals(int[] a, int[] b) {
+        for (int i = 0; i < a.length; i++) {
+            if(a[i] != b[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
